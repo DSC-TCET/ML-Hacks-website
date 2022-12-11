0f8693aacb097430e174c1ad8cde18c16bd6610c
unknown <vanshbaghel912@gmail.com>
2022-12-11 22:49:14 +0530

    Made sponser and events page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Container } from "@mui/material";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
+import EventCard from "./pages/EventCard";
 import Landing from "./pages/Landing";
+import Sponser from "./pages/Sponser";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navbar />
       <Landing />
       <About />
+      <EventCard />
+      <Sponser />
     </Container>
   );
 }
